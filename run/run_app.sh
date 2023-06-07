c4e9906dc6581872154bd1aa925f017270e5ec43
nazam9519 <nazam9519@gmail.com>
2023-06-07 18:20:44 -0400

    changed pycache destination

--- run/run_app.sh
+++ run/run_app.sh
@@ -4,4 +4,6 @@ export PYTHONPATH='/usr/bin/'
 export PVM='/home/nabil/Documents/other'
 export MAINAPP='/opt/rest/'
 export PICONFIG='/etc/pihole'
+export PYTHONPYCACHEPREFIX='/opt/rest/.cache'
+rm -rf $PYTHONPYCACHEPREFIX/*
 exec /usr/bin/python $* $1
